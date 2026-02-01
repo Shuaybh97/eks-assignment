d2a4e7fde8cefdb505c5356a4014346f06d25b19
Shuaib Hussein <93594848+Shuaybh97@users.noreply.github.com>
2026-02-01 15:22:53 +0000

    adding helm apps

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,11 +1,11 @@
 module "networking" {
-  source      = "./modules/networking"
+  source      = "./terraform-modules/networking"
   vpc_cidr    = var.vpc_cidr
   global_tags = local.global_tags
 }
 
 module "eks_cluster_role" {
-  source           = "./modules/iam"
+  source           = "./terraform-modules/iam"
   role_name        = "eks-project-cluster-role"
   service_name     = "eks.amazonaws.com"
   managed_policies = ["AmazonEKSClusterPolicy", "AmazonEKSVPCResourceController"]
@@ -13,7 +13,7 @@ module "eks_cluster_role" {
 }
 
 module "eks_nodes_role" {
-  source           = "./modules/iam"
+  source           = "./terraform-modules/iam"
   role_name        = "eks-project-nodes-role"
   service_name     = "ec2.amazonaws.com"
   managed_policies = ["AmazonEKSWorkerNodePolicy", "AmazonEKS_CNI_Policy", "AmazonEC2ContainerRegistryReadOnly"]
@@ -21,7 +21,7 @@ module "eks_nodes_role" {
 }
 
 module "eks" {
-  source               = "./modules/eks"
+  source               = "./terraform-modules/eks"
   eks_cluster_name     = var.eks_cluster_name
   eks_cluster_role_arn = module.eks_cluster_role.role_arn
   node_group_role_arn  = module.eks_nodes_role.role_arn
@@ -44,15 +44,92 @@ module "eks" {
 #   attach_external_dns_policy    = var.attach_external_dns_policy
 #   external_dns_hosted_zone_arns = var.external_dns_hosted_zone_arns
 #   external_dns_namespace        = var.external_dns_namespace
-
+#
 # }
 
-# module "helm" {
-#   source                     = "./modules/helm"
-#   depends_on_modules         = [module.irsa]
-#   cert_manager_irsa_role_arn = module.irsa.cert_manager_irsa_role_arn
-#   external_dns_irsa_role_arn = module.irsa.external_dns_irsa_role_arn
-#   eks_cluster_name           = module.eks.eks_cluster_name
-#   eks_cluster_endpoint       = module.eks.eks_cluster_endpoint
-#   eks_cluster_ca_data        = module.eks.eks_cluster_certificate_authority
-# }
+
+resource "helm_release" "ingress_nginx" {
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  create_namespace = true
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+    value = "nlb"
+  }
+
+  depends_on = [module.eks]
+}
+
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+
+  depends_on = [module.eks]
+}
+
+resource "helm_release" "external_dns" {
+  name             = "external-dns"
+  repository       = "https://kubernetes-sigs.github.io/external-dns/"
+  chart            = "external-dns"
+  namespace        = "external-dns"
+  create_namespace = true
+
+  set {
+    name  = "provider"
+    value = "aws"
+  }
+
+  set {
+    name  = "policy"
+    value = "upsert-only"
+  }
+
+  depends_on = [module.eks]
+}
+
+resource "helm_release" "kube_prometheus_stack" {
+  name             = "kube-prometheus-stack"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  namespace        = "monitoring"
+  create_namespace = true
+
+  set {
+    name  = "grafana.adminPassword"
+    value = "admin"
+  }
+
+  depends_on = [module.eks]
+}
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "server.extraArgs[0]"
+    value = "--insecure"
+  }
+
+  depends_on = [module.eks]
+}
+
--- Infrastructure/provider.tf
+++ Infrastructure/provider.tf
@@ -18,14 +18,10 @@ terraform {
 }
 
 provider "helm" {
-  kubernetes = {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-      command     = "asws"
-    }
+ kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
