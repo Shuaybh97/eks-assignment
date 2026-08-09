0562fe5482e54a44953da193ced60725234bdd41
Shuaib <shuaibh97@gmail.com>
2026-08-09 12:51:53 +0100

    fmt

--- Infrastructure/terraform/helm/locals.tf
+++ Infrastructure/terraform/helm/locals.tf
@@ -1,10 +1,10 @@
 locals {
   charts = {
-    traefik                              = "traefik"
-    cert_manager                         = "cert-manager"
-    external_dns                         = "external-dns"
-    kube-prometheus-stack                = "kube-prometheus-stack"
-    argocd                               = "argocd"
+    traefik                               = "traefik"
+    cert_manager                          = "cert-manager"
+    external_dns                          = "external-dns"
+    kube-prometheus-stack                 = "kube-prometheus-stack"
+    argocd                                = "argocd"
     secrets-store-csi-driver-provider-aws = "secrets-store-csi-driver-provider-aws"
   }
 }
